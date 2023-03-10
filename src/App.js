29e8175f678856e2df2e1b9861abd900ac457783
Desire <desire_guilarte@hotmail.com>
2023-03-10 12:42:56 -0500

    adding sass, working responsiveness

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const picture1x2 = "https://ucarecdn.com/3e0b15c5-4270-46e5-8104-9443b0b07d3e/Rectangle1x2.png";
+const picture2x2 = "https://ucarecdn.com/353a8d60-7c9f-4564-b87c-a074bd49a928/Rectangle2x2.png";
+const picture3x2 = "https://ucarecdn.com/198d104f-4b6c-4874-b43d-523bfbca0869/Rectangle3x2.png";
+const menu = "https://ucarecdn.com/4be237ac-3186-4f83-abf2-4fdd3c0ffb66/menu.png";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="land-image-1" src={picture1x2}/>
+      <img className="land-image-2" src={picture2x2}/>
+      <img className="land-image-3" src={picture3x2}/>
+      <img className="menu-icon" src={menu} />
+      <div className="land-est">EST 2022</div>
+      <div className="land-logo">SO
+      <div className="land-logo-inner">CANDID</div>
+      PHOTOGRAPHY</div>
+      <div className="land-menu land-menu-about">ABOUT</div>
+      <div className="land-menu land-menu-portfolio">PORTFOLIO</div>
+      <div className="land-menu land-menu-contact">CONTACT</div>
     </div>
   );
 }
